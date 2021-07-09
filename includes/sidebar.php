7d3b86c5431fd6317bfc12c50bb6a7b1d39098a7
kuzgunxp <nz.kerem@gmail.com>
2021-07-09 17:04:55 +1200

    icon change

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -17,10 +17,10 @@
     <i class="far fa-calendar-alt me-2"></i>Time Table
     </a>
     <a href="add-course.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold <?php if($page=='add-course') {echo 'active';} ?>">
-    <i class="fab fa-font-awesome-flag me-2"></i>Add Course
+    <i class="far fa-calendar-plus me-2"></i>Add Course
     </a>
     <a href="add-time-table.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold <?php if($page=='add-time-table') {echo 'active';} ?>">
-    <i class="far fa-calendar-plus me-2"></i>Add Class
+    <i class="fab fa-font-awesome-flag me-2"></i>Add Class
     </a>
     <a href="holidays.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold <?php if($page=='holidays') {echo 'active';} ?>">
     <i class="fas fa-plane me-2"></i>Holidays
