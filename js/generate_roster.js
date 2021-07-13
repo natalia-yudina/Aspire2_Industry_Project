98e465369b727836638439f5e3220b65f2bf33b5
natalia-yudina <yudina.nz@mail.ru>
2021-07-14 11:03:58 +1200

    Update generate_roster.js
    updating events by Generate button

--- js/generate_roster.js
+++ js/generate_roster.js
@@ -2,9 +2,33 @@ $(document).ready(function() {
 
     // Button "Generate roster" click begin
      $(document).on('click', '.generate_roster', function() {
-       // $('#calendar').fullCalendar('rerenderEvents');
-       // $('#calendar').fullCalendar('refetchEvents');
-       // console.log("lkk");
+
+       $.ajax({
+ 				url	: 'api/process.php?cmd=rosterview',
+ 				dataType: 'json',
+ 				type	: 'POST',
+ 				data	: {
+ 					start	: $('#calendar').fullCalendar('getView').intervalStart.format(),
+ 					end		: $('#calendar').fullCalendar('getView').intervalEnd.format()
+ 				},
+        success: function(response) {
+
+          //remove old data
+          $('#calendar').fullCalendar('removeEvents');
+
+          //Getting new event json data
+          $("#calendar").fullCalendar('addEventSource', response);
+
+          //Updating new events
+          // $('#calendar').fullCalendar('rerenderEvents');
+
+          //getting latest Events
+          //$('#fullCalendar').fullCalendar( 'refetchEvents' );
+
+          //getting latest Resources
+          $('#calendar').fullCalendar( 'refetchResources' );
+ 				}
+ 			});
 
     });
     // Button "Generate roster" click end
