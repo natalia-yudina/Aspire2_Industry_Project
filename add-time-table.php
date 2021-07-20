5dddcfb262080651d717ba85d433f487e2b78a89
natalia-yudina <yudina.nz@mail.ru>
2021-07-20 16:08:41 +1200

    Update add-time-table.php
    form-select class for Head Coach and Course

--- add-time-table.php
+++ add-time-table.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 
 if (isset($_SESSION['email']))
@@ -20,15 +20,15 @@ if (isset($_SESSION['email']))
 </head>
 
 <body>
-<?php  
+<?php
   if ($_SESSION['role'] == '2')
    { ?>
     <div class="d-flex" id="wrapper">
         <!-- Sidebar -->
         <?php $page = 'add-time-table';  include('includes/sidebar.php'); ?>
         <!-- Sidebar END -->
-        
-        
+
+
         <div id="page-content-wrapper">
             <!-- Top Navbar -->
             <?php include('includes/nav.php'); ?>
@@ -39,12 +39,12 @@ if (isset($_SESSION['email']))
                     <div class="col-md-4 py-3">
 <!-- NI 03-06-2021 begin -->
 <!-- <form action="/action_page.php"> -->
-<h5 class="px-4"><i class="fas fa-circle ui-text p-2 fs-6 "></i>Add</h5> 
+<h5 class="px-4"><i class="fas fa-circle ui-text p-2 fs-6 "></i>Add</h5>
 <form class="px-4">
 <!-- NI 03-06-2021 end -->
 <div class="col-md-12 form-group py-2">
     <label for="hcname">Who is Head Coach?</label>
-    <select class="form-control" id="hcname">
+    <select class="form-control form-select" id="hcname">
       <!-- NI 03-06-2021 begin -->
 			<!-- <option>Jane Doe</option>
       <option>Jhone Doe</option> -->
@@ -65,7 +65,7 @@ if (isset($_SESSION['email']))
   </div>
   <div class="col-md-12 form-group pt-3">
     <label for="cname">Choose the Course</label>
-    <select class="form-control" id="cname">
+    <select class="form-control form-select" id="cname">
       <!-- NI 03-06-2021 begin -->
             <!-- <option>Blue Fleet</option>
             <option>Green Fleet</option> -->
@@ -96,7 +96,7 @@ if (isset($_SESSION['email']))
       <option>Sunday</option>
     </select>
   </div>
-  
+
   <div class="row pb-2">
   <div class="col-sm-6 form-group pt-3">
       <label for="stime">Start</label>
@@ -107,23 +107,23 @@ if (isset($_SESSION['email']))
       <input type="time" class="form-control" id="etime" name="etime">
     </div>
     </div>
-    
+
     <!-- NI 03-06-2021 begin -->
         <!-- <button type="submit" class="btn btn-dark mt-4 mb-5 "> Add Course </button> -->
-        
+
         <div class="col-md-12">
         <button type="button" class="btn add_new_class my-4 py-3  w-100" onClick="window.location.reload();"> Add Class<i class="fas fa-plus px-2"></i></button>
         </div>
-    		
+
     <!-- NI 03-06-2021 end -->
   </form>
 
- 
+
                     </div>
                     <div class="col-md-8 px-4 py-3">
-        
+
         <?php include('classeslist.php'); ?>
-                   
+
   </div>
                 </div>
             </div>
@@ -132,10 +132,10 @@ if (isset($_SESSION['email']))
     </div>
 
     <!-- Sidebar JS -->
-    <script type = "text/javascript" src="js/sidebar.js"></script>  
+    <script type = "text/javascript" src="js/sidebar.js"></script>
     <!-- Bootstrap Bundle -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
     <script src="js/add_class.js"></script>
     <?php } ?>
