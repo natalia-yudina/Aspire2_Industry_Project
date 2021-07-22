e2025a9beb525127f61c0270eb06c5e3a0ba13f7
natalia-yudina <yudina.nz@mail.ru>
2021-07-22 15:31:07 +1200

    fixed: keep DB connection settings in one place (config.php)
    db_conn.php is no longer used

--- includes/check_login.php
+++ includes/check_login.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 session_start();
-include "db_conn.php";
+// include "db_conn.php";
+require_once '../config.php';
 
-if (isset($_POST['email']) && isset($_POST['password'])) 
+if (isset($_POST['email']) && isset($_POST['password']))
 {
 
     function test_input($data) {
@@ -25,44 +26,44 @@ if (isset($_POST['email']) && isset($_POST['password']))
         $password = md5($password);
 
         $sql = "SELECT * FROM users where email='$email' AND password='$password'";
-        $result = mysqli_query($conn, $sql);
-         
+        $result = mysqli_query($dbConn, $sql);
+
         if (mysqli_num_rows($result)===1) {
             $row = mysqli_fetch_assoc($result);
-            if ($row['email']===$email && $row['password']===$password && $row['id_role'] !== '2') 
+            if ($row['email']===$email && $row['password']===$password && $row['id_role'] !== '2')
             {
-              
+
                 $_SESSION['id'] = $row['id_user'];
                 $_SESSION['fname'] = $row['first_name'];
                 $_SESSION['lname'] = $row['last_name'];
                 $_SESSION['email'] = $row['email'];
-                $_SESSION['phone'] = $row['phone_number'];   
+                $_SESSION['phone'] = $row['phone_number'];
                 $_SESSION['address'] = $row['address'];
                 $_SESSION['role'] = $row['id_role'];
 
                header("Location: ../my-profile.php");
-            } 
-            
-            
+            }
+
+
             else {
-                if ($row['email']===$email && $row['password']===$password && $row['id_role'] !== '1') 
+                if ($row['email']===$email && $row['password']===$password && $row['id_role'] !== '1')
             {
-           
+
                 $_SESSION['id'] = $row['id_user'];
                 $_SESSION['fname'] = $row['first_name'];
                 $_SESSION['lname'] = $row['last_name'];
                 $_SESSION['email'] = $row['email'];
-                $_SESSION['phone'] = $row['phone_number'];   
+                $_SESSION['phone'] = $row['phone_number'];
                 $_SESSION['address'] = $row['address'];
                 $_SESSION['role'] = $row['id_role'];
 
                header("Location: ../roster.php");
-            } 
+            }
                  }
 
 
-            } 
-            
+            }
+
             else {
                 header("Location:../signin.php?error=Incorrect Email or password");
                  }
